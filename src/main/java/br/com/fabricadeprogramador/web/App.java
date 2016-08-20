9c81ede0d5d7e40129ecb664cdf5f738ff70ed94
HudsonCampos <hudsonsdi@gmail.com>
2016-08-19 22:41:39 -0300

    first commit

---
+++ src/main/java/br/com/fabricadeprogramador/web/App.java
@@ -0,0 +1,13 @@
+package br.com.fabricadeprogramador.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
